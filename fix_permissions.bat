c75abd1b9b01944c3350cc069175166b7d7a8e65
cyberspherecu-cyber <cybersphere.cu@gmail.com>
2026-08-16 04:04:15 +0530

    Initial commit

--- fix_permissions.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Granting full control to current user on F:\bihari...
-icacls "F:\bihari" /grant Everyone:(OI)(CI)F /T /C
-echo Copying files from C:\Users\lenovo\bihari to F:\bihari...
-xcopy "C:\Users\lenovo\bihari\*" "F:\bihari\" /E /I /Y
-echo Done! Bihari Orchestra is ready in F:\bihari.
-pause
